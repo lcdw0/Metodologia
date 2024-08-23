19d25dbc5ebed738b87cf0c159aca97785393c4f
Mario <mjcarrion@uamv.edu.ni>
2024-08-23 08:41:29 -0600

    Sesion de clases 23 de agosto 2024

---
+++ Clase23.08/Compras/Compras/Program.cs
@@ -0,0 +1,20 @@
+﻿using Compras.clases;
+
+namespace Compras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejemplo 1");
+            Factura fact = new Factura();
+            string producto = null;
+            double precio = 0, monto;
+            int cantidad = 0;
+            Console.WriteLine($"{producto}");
+            Console.WriteLine($"{precio}");
+            Console.WriteLine($"{cantidad}");
+            //Console.WriteLine($"{monto}");
+        }
+    }
+}
---
+++ Uam/Uam/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Uam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
