5e3688aa85d0abcb2e7861841ab896c26c8948be
Mario <mjcarrion@uamv.edu.ni>
2024-08-26 07:19:02 -0600

    Quinto ejercicio

---
+++ Ejercico #5/Ejercico #5/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ejercico__5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definir las variables
+            string nombre, cargo;
+            double salr, result;
+            
+            //Bienvenida
+            Console.WriteLine("Bienvenido");
+
+            //Registrar datos
+            Console.WriteLine("Ingrese su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su cargo");
+            cargo = Console.ReadLine();
+            Console.WriteLine("Digite su salario sin comas");
+            salr = double.Parse(Console.ReadLine());
+            result = salr*1.10;
+
+            //Resultados
+            Console.WriteLine("Su nombre es: " + nombre );
+            Console.WriteLine("Su cargo es: " + cargo );
+            Console.WriteLine("Su salario mas el 10 por ciento aplicado es: "+ result);
+        }
+    }
+}
