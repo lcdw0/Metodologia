95df4b6f901b3a3849f2a372f511b77761105f88
Mario <mjcarrion@uamv.edu.ni>
2024-08-26 07:18:40 -0600

    Tercer ejercicio

---
+++ Ejercicio #3/Ejercicio #3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ejercicio__3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definicion de variables
+            float num1, num2, result;
+
+            Console.WriteLine("Sumar dos numeros");
+            Console.WriteLine("Ingrese primer numero entero");
+            num1= float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero entero");
+            num2= float.Parse(Console.ReadLine());
+
+            result = num1 + num2;
+            Console.WriteLine("El resultado de la suma es " + result);
+            
+        }
+    }
+}
