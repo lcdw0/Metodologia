a9109b7d73d6611dbd7db8b31f64a23377141aa8
Mario <mjcarrion@uamv.edu.ni>
2024-08-26 07:18:52 -0600

    Cuarto ejercicio

---
+++ Ejercicio #4/Ejercicio #4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ejercicio__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definicion de variables
+            double rad, result;
+
+            Console.WriteLine("Calculo del area de un circulo");
+            Console.WriteLine("Ingrese el radio del circulo");
+            rad=double.Parse(Console.ReadLine());
+            
+            result= Math.PI*Math.Pow(2,rad);
+            Console.WriteLine("El area del circulo es igual a " + result);
+
+            
+        }
+    }
+}
