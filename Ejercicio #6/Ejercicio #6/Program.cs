d5d72287ebc6779fa8f0a3c7979d3d0d7e074c1b
Mario <mjcarrion@uamv.edu.ni>
2024-08-26 07:19:17 -0600

    Sexto ejercicio

---
+++ Ejercicio #6/Ejercicio #6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Design;
+
+namespace Ejercicio__6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definir numero
+            int num;
+
+            //Escribir numero
+            Console.WriteLine("Escriba un numero entero");
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Su numero es par");
+            }
+            else
+            {
+                Console.WriteLine("Su numero es impar");
+            }
+        }
+    }
+}
