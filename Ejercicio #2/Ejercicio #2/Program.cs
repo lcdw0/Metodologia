3615467a013c7cfe9bb41e9b67c5f1cda9dae3c6
Mario <mjcarrion@uamv.edu.ni>
2024-08-26 07:18:28 -0600

    Segundo ejercicio

---
+++ Ejercicio #2/Ejercicio #2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ejercicio__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración de variables;
+            string nombre;
+            //Leer el nombre y saludar
+            Console.WriteLine("Hola, bienvenido!");
+            Console.Write("Digite su nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre + ", un gusto!");
+
+        }
+    }
+}
